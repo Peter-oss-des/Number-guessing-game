c3752b6eb1601005fecf06e21bdfe6ec9b6e59c0
Peter-oss-des <68293221+Peter-oss-des@users.noreply.github.com>
2023-10-17 15:07:22 +0000

    Created basic functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
@@ -9,11 +10,27 @@ def index():
 
 @app.route('/process', methods = ['POST'])
 def process():
+    solution_lst = []
+    score = 0
+    for i in range(6):
+      correct_number = random.randint(0,222)
+      solution_lst.append(correct_number)
+    print(solution_lst)
+  
     
-    return render_template('result.html')
+    guess = [request.form[f'guessField{number}'] for number in range(6)]
+  
+    print(guess)
+    for i in range(6):
+      if guess[i] == solution_lst[i]:
+        score = score + 1
+
+    score_results = f'Your score is {score} out of 6.'
+    
+    return render_template('result.html',processed_data = score_results)
   
   
 
 if __name__ == '__main__':
   app.debug = True
-  app.run(host='0.0.0.0', port=3000)
+  app.run(host='0.0.0.0', port=5000)
